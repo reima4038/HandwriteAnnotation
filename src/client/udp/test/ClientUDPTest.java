2fb42f4fc69e722fe3d077056eb480140c582ab5
Reima Shuke <g2112020@fun.ac.jp>
2012-11-11 18:06:17 +0900

    UDP通信テストクラスのスケルトンを用意

--- src/client/udp/test/ClientUDPTest.java
+++ src/client/udp/test/ClientUDPTest.java
@@ -7,8 +7,39 @@ import org.junit.Test;
 public class ClientUDPTest {
 
 	@Test
-	public void test() {
+	/**
+	 * 一度開放されたソケット通信が閉じられているか
+	 */
+	public void socketCloseTest() {
 		fail("Not yet implemented");
 	}
-
+	
+	/**
+	 * パケットが正常に送信されているか
+	 */
+	public void packetSendTest() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * パケットを正常に受信できたか
+	 */
+	public void packetReceiveTest() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * 受信したパケットのバイトコードをデコードできるか
+	 */
+	public void packetDecodeTest() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * 送信するパケットをバイトコードにできるか
+	 */
+	public void packetCodeTest() {
+		fail("Not yet implemented");
+	}
+	
 }
